e7bdba3911be804cd100715836adc8407c3b2883
ultimate_hecker <86735991+ultimatehecker@users.noreply.github.com>
2024-06-19 10:52:04 -0400

    fixed TypeErrors

--- src/commands/general/latency.js
+++ src/commands/general/latency.js
@@ -1,10 +1,12 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, Discord, Interaction } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, Interaction } = require('discord.js');
+const colors = require('../../tools/colors.json');
+const Discord = require('discord.js');
 
 module.exports = {
   name: 'latency',
   description: 'Reponds with the latency from the Discord API and myself',
-  //devOnly: false,
-  //testOnly: true,
+  devOnly: false,
+  testOnly: false,
   //options: Object[],
   deleted: false,
 
@@ -15,50 +17,51 @@ module.exports = {
    */
   callback: async (client, interaction) => {
     await interaction.deferReply({ ephemeral: false });
-    const latencyTest = new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel('Retry').setCustomId('Retry').setStyle(ButtonStyle.PRIMARY));
+    const latencyTest = new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel('Retry').setCustomId('Retry').setStyle(ButtonStyle.Primary));
 
-    const authorSuccess = {
+    let authorSuccess = {
       name: "Bot & API Latency",
-      iconURL: "https://cdn.discordapp.com/avatars/1148422189129941032/05c7d704c2a36b00f7c08d25fe16675c.webp",
+      iconURL: "https://cdn.discordapp.com/avatars/1148422189129941032/05c7d704c2a36b00f7c08d25fe16675c.webp"
     }
 
-    const authorError = {
+    let authorError = {
       name: "Error",
-      iconURL: "https://cdn.discordapp.com/avatars/1148422189129941032/05c7d704c2a36b00f7c08d25fe16675c.webp",
-    }
+      iconURL: "https://cdn.discordapp.com/avatars/1148422189129941032/05c7d704c2a36b00f7c08d25fe16675c.webp"
+    }	
+		
+		let latestEmbed = new Discord.EmbedBuilder()
+			.setAuthor(authorSuccess)
+			.setDescription(`Heckernetic's Latency is \`${Date.now() - interaction.createdTimestamp}\` ms. \n Discord API Latency is \`${Math.round(client.ws.ping)}\` ms.`)
+			.setColor(colors["MainColor"]);
 
-    let latencyEmbed = new Discord.EmbedBuilder()
-      .setAuthor(authorSuccess)
-      .setDescription(`Heckernetic's Latency is: \`${Date.now() - interaction.createdTimestamp}\`ms. \n The Discord API Latency is: \`${Math.round(client.ws.ping)}\`ms.`)
-      .setColor('#00FF00');
+		interaction.editReply({embeds: [latestEmbed], allowedMentions: { repliedUser: true }, components: [latencyTest] }).then((reply) => {
+      let collector = reply.createMessageComponentCollector({ time: 300000, dispose: true });
+      collector.on("collect", async (press) => {
+          if (press.user.id !== interaction.user.id) {
+              const unauthorizedAction = new Discord.EmbedBuilder()
+                  .setAuthor(authorError)
+                  .setColor(colors["MainColor"])
+                  .setDescription("You cannot perform this action!");
 
-      interaction.editReply({embeds: [latencyEmbed], allowedMentions: { repliedUser: true }, components: [latencyTest] }).then((reply) => {
-        let collector = reply.createMessageComponentCollector({ time: 300000, dispose: true });
-        collector.on("collect", async (press) => {
-          if(press.user.id != interaction.user.id) {
-            const unauthorizedAction = new Discord.EmbedBuilder()
-              .setAuthor(authorError)
-              .setDescription(`You are not authorized to perform this action.`)
-              .setColor('#FF0000');
-
-            press.reply({ embeds: [unauthorizedAction], allowedMentions: { repliedUser: false } }).then(() => {
-              setTimeout(function() {
-                interaction.deleteReply();
-              }, 5000)
-            })
+              press.reply({ embeds: [unauthorizedAction], allowedMentions: { repliedUser: true } }).then(() => {
+                  setTimeout(function() {
+                      interaction.deleteReply()
+                  }, 5000);
+              });
           } else {
-            await press.deferUpdate();
-            const retestedLatencyEmbed = new Discord.EmbedBuilder()
-              .setAuthor(authorSuccess)
-              .setDescription(`Heckernetic's Latency is: \`${Date.now() - interaction.createdTimestamp}\`ms. \n The Discord API Latency is: \`${Math.round(client.ws.ping)}\`ms.`)
-              .setColor('#00FF00');
+              await press.deferUpdate();
 
-            latencyEmbed = retestedLatencyEmbed;
-            press.editReply({ embeds: [retestedLatencyEmbed], allowedMentions: { repliedUser: true } });
+              const embed = new Discord.EmbedBuilder()
+                  .setAuthor(authorSuccess)
+                  .setColor(colors["MainColor"])
+                  .setDescription(`Heckernetic's Latency is \`${Date.now() - press.createdTimestamp}\` ms. \n Discord API Latency is \`${Math.round(client.ws.ping)}\` ms.`)
+                  
+              latestEmbed = embed;
+              press.editReply({ embeds: [embed], allowedMentions: { repliedUser: true } });
           }
-        });
-
-        collector.on("end", () => { interaction.editReply({ embeds: [latencyEmbed], allowedMentions: { repliedUser: true }, components: [latencyTest] }) });
       });
+
+      collector.on("end", () => {interaction.editReply({ embeds: [latestEmbed], allowedMentions: { repliedUser: true }, components: [latencyTest] }) });
+    });
   },
 };

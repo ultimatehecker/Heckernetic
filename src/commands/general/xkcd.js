ab41a40a116b7f67fe04e6ede387c80b22d237c8
ultimate_hecker <86735991+ultimatehecker@users.noreply.github.com>
2024-06-28 12:07:03 +0530

    xkcd command

---
+++ src/commands/general/xkcd.js
@@ -0,0 +1,52 @@
+const { ApplicationCommandOptionType, Client, Interaction } = require('discord.js');
+const colors = require('../../tools/colors.json');
+const Discord = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    name: 'xkcd',
+    description: 'Responds with the xkcd of the day or the specified comic numbers',
+    devOnly: true,
+    testOnly: false,
+    options: [
+    {
+        name: 'number',
+        description: 'The comic number you want to see',
+        type: ApplicationCommandOptionType.Integer,
+        required: false,
+    }
+    ],
+    deleted: false,
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Interaction} interaction 
+     */
+    callback: async (client, interaction) => {
+        await interaction.deferReply({ ephemeral: false });
+        const number = interaction.options.get("number")?.value;
+
+        let author = {
+            name: "Randall Munroe",
+            iconURL: "https://cdn.discordapp.com/avatars/1148422189129941032/05c7d704c2a36b00f7c08d25fe16675c.webp"
+        }
+
+        let comic;
+        if(number) {
+            comic = (await axios.get(`https://xkcd.com/${number}/info.0.json`)).data;  
+        } else {
+            comic = (await axios.get(`https://xkcd.com/info.0.json`)).data;
+        }
+
+        const xkcdEmbed = new Discord.EmbedBuilder()
+            .setTitle(`XKCD #${comic.num} - ${comic.title}`)
+            .setAuthor(author)
+            .setColor(colors["MainColor"])
+            .setImage(comic.img)
+            .setDescription(comic.alt)
+            .setFooter({ text: `Published on ${comic.month}/${comic.day}/${comic.year}` });
+
+        interaction.editReply({ embeds: [xkcdEmbed], allowedMentions: { repliedUser: true } });
+    }
+}

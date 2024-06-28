b81b9f3d27c1bc58df24bb2e1bd41717168c4e6b
ultimate_hecker <86735991+ultimatehecker@users.noreply.github.com>
2024-06-28 14:50:58 +0530

    two new commands

---
+++ src/commands/general/discord.js
@@ -0,0 +1,37 @@
+const {ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, Interaction} = require('discord.js');
+const colors = require(`../../tools/colors.json`);
+const Discord = require('discord.js');
+const { callback } = require('./info');
+
+module.exports = {
+    name: "discord",
+    description: "Reponds with a link to invite yourself to the Support Server.",
+    devOnly: false,
+    testOnly: false,
+    //options: Object[],
+    deleted: false,
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Interaction} interaction 
+     */
+
+    callback: async (client, interaction) => {
+        await interaction.deferReply({ ephemeral: false });
+
+        let author = {
+            name: "Heckernetic Support Server",
+            iconURL: "https://cdn.discordapp.com/avatars/1148422189129941032/05c7d704c2a36b00f7c08d25fe16675c.webp"
+        }
+
+        const discordInvite = new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Support Server").setStyle(ButtonStyle.Link).setURL('https://discord.gg/4hv23VguQY'))
+
+        const discord = new Discord.EmbedBuilder()
+            .setAuthor(author)
+            .setColor(colors["MainColor"])
+            .setDescription('Heckernetic has a support server where you can ask questions, get help, report bugs, and suggest features. Join the server by clicking the button below.')
+
+        interaction.editReply({ embeds: [discord], components: [discordInvite], allowedMentions: { repliedUser: true } });
+    }
+}
---
+++ src/commands/general/invite.js
@@ -0,0 +1,36 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, Interaction } = require('discord.js');
+const colors = require(`../../tools/colors.json`);
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "invite",
+    description: "Responds with a link to invite Heckernetic to your server.",
+    devOnly: false,
+    testOnly: false,
+    //options: Object[],
+    deleted: false,
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Interaction} interaction 
+     */
+
+    callback: async (client, interaction) => {
+        await interaction.deferReply({ ephemeral: false });
+
+        let author = {
+            name: "Heckernetic Invite Link",
+            iconURL: "https://cdn.discordapp.com/avatars/1148422189129941032/05c7d704c2a36b00f7c08d25fe16675c.webp"
+        }
+
+        const botInvite = new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Invite Heckernetic").setStyle(ButtonStyle.Link).setURL('https://discord.com/oauth2/authorize?client_id=1148422189129941032&permissions=8&integration_type=0&scope=applications.commands+bot'));
+
+        const invite = new Discord.EmbedBuilder()
+            .setAuthor(author)
+            .setColor(colors["MainColor"])
+            .setDescription('Invite Heckernetic to your server by clicking the button below. Heckernetic requires the Administrator permission to function properly.')
+
+        interaction.editReply({ embeds: [invite], components: [botInvite], allowedMentions: { repliedUser: true } });
+    }
+}
